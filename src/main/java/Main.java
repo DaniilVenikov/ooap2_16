3fb74a40aa3679ea5d4ef34df535154eff431caf
DaniilVenikov <daniilvenikoff@gmail.com>
2025-03-12 09:08:30 +0300

    Refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,71 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Bird();
-        List<Bird> list = new ArrayList<>();
-        list.add(new Bird());
+        Zoo zoo = new ZooBird();
 
-        animal.someMethod(list);
+        List<Animal> list = new ArrayList<>();
+        list.add(new Bird());
+        list.add(new Dog());
+
+        zoo.covariantMethod(list);
+        zoo.polymorphicMethod(new Bird());
     }
 }
 
 class Animal {
-    public <T extends Animal> void someMethod(List<T> list) {
-        System.out.printf("This animal list %s", list.toString());
+    public void method() {
+        System.out.println("Animal");
     }
 }
 
 
 class Bird extends Animal {
     @Override
-    public <T extends Animal> void someMethod(List<T> list) {
-        System.out.printf("This bird list %s", list.toString());
+    public void method() {
+        System.out.println("Bird");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void method() {
+        System.out.println("Dog");
+    }
+}
+
+class Zoo {
+    public <T extends Animal> void covariantMethod(List<T> animals) {
+        for(T animal : animals) {
+            System.out.println(animal.toString());
+        }
+    }
+
+    public void polymorphicMethod(Animal animal) {
+        animal.method();
+    }
+
+    Animal getAnimal() {
+        System.out.println("Get animal");
+        return new Animal();
+    }
+}
+
+class ZooBird extends Zoo {
+    @Override
+    public <T extends Animal> void covariantMethod(List<T> animals) {
+            super.covariantMethod(animals);
+            System.out.println(animals.size());
+    }
+
+    @Override
+    public void polymorphicMethod(Animal animal) {
+        animal.method();
+    }
+
+    @Override
+    Bird getAnimal() {
+        System.out.println("Get animal");
+        return new Bird();
     }
 }
 
